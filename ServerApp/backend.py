0332212448337ee5ccbdec314a4808bf8639bb23
Maja06063 <259707@student.pwr.edu.pl>
2023-09-05 13:54:47 +0200

    działające logowanie, praca nad cardami

--- ServerApp/backend.py
+++ ServerApp/backend.py
@@ -8,19 +8,33 @@ app = Flask(__name__) #tworzenie nowej instancji klasy Flask
 def initial_web_page():
     return render_template("index.html")
 
+@app.route('/podstrona_login', methods=['GET'])
+def redirect_to_login_page():
+    return "<script>location.href = '/';</script>"
+
+
 @app.route('/podstrona_login', methods=['POST'])
 def logged_page():
     post_data_dict = request.form.to_dict()
     cursor = connection.cursor()
     cursor.execute("SELECT * FROM UserName WHERE login = '" + post_data_dict["login"] + "';")
-    
     rows = cursor.fetchall()
 
-    for row in rows:
-        print(row)
+    if (len(rows) == 1):
 
-    return generate_logged_page(post_data_dict)
+        if (str(rows[0][1])==str(post_data_dict["password"])):
+            return generate_logged_page(post_data_dict, connection)
+
+        return render_template("index.html") + """
+            <script>var pass_error = document.getElementById("pass_error");
+            pass_error.style.display = "block";</script>
+            """
+
+    return render_template("index.html") + """
+        <script>var login_error = document.getElementById("login_error");
+        login_error.style.display = "block";</script>
+        """
 
 if __name__ == '__main__':
     connection = base_connect()
-    app.run(debug=True)
+    app.run()
--- ServerApp/podstrona_login.py
+++ ServerApp/podstrona_login.py
@@ -1,4 +1,38 @@
+from flask import render_template
 
-def generate_logged_page(args) -> str:
+def generate_one_card(plant) -> str:
 
-    return "Tu będzie strona po zalogowaniu. Parametry:<br>" + str(args)
+    card = """
+        <div class="card">
+            <center>
+                <h2 class="no-bold-header">%s</h2>
+                <span style="display: block";> Następne podlewanie: </span>
+                <span style="display: block";> Następne nawożenie: </span>
+                <span style="display: block";> Następne wydarzenie: </span>
+            </center>
+        </div>
+        """ % plant[3]
+
+    return card
+
+def generate_plants_cards(plants) -> str:
+
+    cards = ""
+    for plant in plants:
+        cards += generate_one_card(plant)
+
+    cards_script = """<script>var plant_cards = document.getElementById("plant_cards");
+        plant_cards.innerHTML = `%s`;</script>""" % cards
+
+    return cards_script
+
+def generate_logged_page(args_dict, connection) -> str:
+
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM Plants WHERE login = '" + args_dict["login"] + "';")
+    plants = cursor.fetchall()
+
+    print(plants)
+
+
+    return render_template("podstrona_login.html") + generate_plants_cards(plants)
