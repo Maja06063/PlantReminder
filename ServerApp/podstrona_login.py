d5a336145b381b86c2e07be023ff96b600212621
Maja06063 <259707@student.pwr.edu.pl>
2023-09-09 16:42:03 +0200

    praca nad cardami roślin

--- ServerApp/podstrona_login.py
+++ ServerApp/podstrona_login.py
@@ -1,25 +1,34 @@
 from flask import render_template
 
-def generate_one_card(plant) -> str:
+def generate_one_card(plant,connection) -> str:
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM Species WHERE species_id = '" + str(plant[2])+ "';")
+    species = cursor.fetchall()
+    species=list(species[0])
+    plant_name=species[1]
+    if None in plant[4:7]:
+        for i in range(4,7):
+            if plant[i]==None:
+                plant[i]=species[i-2]
 
     card = """
         <div class="card">
             <center>
                 <h2 class="no-bold-header">%s</h2>
-                <span style="display: block";> Następne podlewanie: </span>
-                <span style="display: block";> Następne nawożenie: </span>
-                <span style="display: block";> Następne wydarzenie: </span>
+                <span style="display: block";> Następne podlewanie:%d </span>
+                <span style="display: block";> Następne nawożenie:%d </span>
+                <span style="display: block";> Następne wydarzenie:%d </span>
             </center>
         </div>
-        """ % plant[3]
+        """ % (plant_name,plant[4],plant[5],plant[6])
 
     return card
 
-def generate_plants_cards(plants) -> str:
+def generate_plants_cards(plants,connection) -> str:
 
     cards = ""
     for plant in plants:
-        cards += generate_one_card(plant)
+        cards += generate_one_card(list(plant),connection)
 
     cards_script = """<script>var plant_cards = document.getElementById("plant_cards");
         plant_cards.innerHTML = `%s`;</script>""" % cards
@@ -32,7 +41,6 @@ def generate_logged_page(args_dict, connection) -> str:
     cursor.execute("SELECT * FROM Plants WHERE login = '" + args_dict["login"] + "';")
     plants = cursor.fetchall()
 
-    print(plants)
 
 
-    return render_template("podstrona_login.html") + generate_plants_cards(plants)
+    return render_template("podstrona_login.html") + generate_plants_cards(plants,connection)
