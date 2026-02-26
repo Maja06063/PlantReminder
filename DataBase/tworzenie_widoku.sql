7cbb426d07bb8f12fc13a3900648399a4602aa2f
Maja <259707@student.pwr.edu.pl>
2026-02-26 18:31:23 +0100

    Change SQL files for UTF-8 and using with mysql CLI

--- DataBase/tworzenie_widoku.sql
+++ DataBase/tworzenie_widoku.sql
@@ -1,7 +1,7 @@
-USE plantreminderdb;
+-- USE plantreminderdb;
 
-DROP VIEW EventOfUsers;
-DROP VIEW PlantNamesSpecies;
+DROP VIEW IF EXISTS EventOfUsers;
+DROP VIEW IF EXISTS PlantNamesSpecies;
 
 CREATE VIEW EventsOfUsers AS
 SELECT SE.special_event_id, SE.event_name, SE.event_description, SE.event_date, P.plant_name, S.name, P.login FROM SpecialEvent SE, Plants P, Species S
